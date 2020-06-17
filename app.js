1b9f828e59fc301af1fd53a0d0d73af5da243cb7
Michael Focacci <michaelfocacci@computer.lan>
2020-06-16 23:26:13 -0400

    Included Express in server

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const express = require('express');
+
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 8888;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+app.get('/', function (req, res) {
+    res.send("Hello world")
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
