7f62eca73d2c517116dbb73958fb30459e84da81
Michael Focacci <michaelfocacci@computer.lan>
2020-06-18 22:34:48 -0400

    began friendsync logic

---
+++ backend/friendsync.js
@@ -0,0 +1,25 @@
+// Friendsync implementations go here
+
+
+module.exports = {
+
+    /**
+    * Invites user to sync playback
+    *   
+    * @argument userid Spotify user ID of desired user
+    * @returns true if user is active and accepts, false otherwise
+    */
+    invite: function (userid) {
+        
+        return "No";
+    }
+};
+
+
+/**
+ * Checks if user with userid is currently connected to this app
+ * 
+ * @argument userid Spotify user ID of desired user
+ * @returns 
+ */
+function is_active(userid) {}
