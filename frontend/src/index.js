1d6f81822e6ecce647a8baee1908f699bcda2402
Kenny <oseleonose@gmail.com>
2020-06-21 04:48:15 +0000

    cleanup changes to make merge possible

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import logo from './logo.svg';
-import './Components/MusicPlayer'
-import MusicPlayer from './Components/MusicPlayer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <MusicPlayer />
   </React.StrictMode>,
   document.getElementById('root')
 );
