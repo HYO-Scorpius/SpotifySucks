22be3a2673b7dc0b64b17ec343dcd9943de662a5
Kenny <oseleonose@gmail.com>
2020-06-21 20:36:12 +0000

    frontend app.js fetches user and playlists from spotify api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,69 @@
 import React, { useState, useEffect } from 'react';
+import SWA from 'spotify-web-api-node';
 import './App.css';
 import Shuffle from './components/Shuffle';
 import MusicPlayer from './components/MusicPlayer'
 import Tabs from './components/Tabs';
 import FriendSyncTab from './components/FriendSyncTab';
-import MultiPlaylistTab from './components/MultiPlaylistTab'
+import MultiPlaylistTab from './components/MultiPlaylistTab';
+import { getCookie } from './helper';
+const spotifyApi = new SWA();
 
 function App() {
+   const [apiToken, setApiToken] = useState(null);
+   const [user, setUser] = useState({});
    const [playlists, setPlaylists] = useState([]);
+
+
+   useEffect(() => {
+      //regularly get api token
+      const token = getCookie('api_token') || null;
+      if (token) {
+         setApiToken(token);
+         spotifyApi.setAccessToken(token);
+      }
+    },[]);
+
+   useEffect(() => {
+     //regularly get user
+      spotifyApi.getMe().then(
+         (data) => {
+            if (data.body) {
+               setUser(data.body);
+            }
+         },
+         (err) => {
+            console.log('frontend::App.js spotifyApi.getMe() failed. Error: ', err);
+         });
+
+      //regularly get user playlist
+      spotifyApi.getUserPlaylists().then(
+         (data) => {
+            if (data.body) {
+               setPlaylists(data.body.items);
+            }
+         },
+         (err) => {
+            console.log('frontend::App.js spotifyApi.getUserPlaylists() failed. Error: ', err);
+         });
+
+   },[apiToken, user]);
+
+
+
 
   return (
      <div>
       <Tabs>
         <div label="Shuffle">
           <Shuffle
-           playlists = {["kenny", "syed", "amy", "mike"]}/>
+           playlists = {playlists}/>
         </div>
         <div label="MultiPlaylist">
-        <MultiPlaylistTab>
-          </MultiPlaylistTab>
+        <MultiPlaylistTab />
         </div>
         <div label="Friendsync">
-        <FriendSyncTab>
-          </FriendSyncTab>
+        <FriendSyncTab />
         </div>
         <div label="VanillaPlaylist">
           leaving empty until the other three are done
