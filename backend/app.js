22ee9f20850c2c58c1a9c989896abbe9bd6b0375
Kenny <oseleonose@gmail.com>
2020-06-21 17:10:52 +0000

    fixed backend merge conflicts

--- backend/app.js
+++ backend/app.js
@@ -21,69 +21,7 @@ app.use(function (req, res, next) {
 
 app
    .use(express.static(path.join('..', 'frontend', 'build')))
-   .use(cookieParser());;
-
-
-var scopes = ['user-read-private', 'user-read-email'],
-   clientID = process.env.SPOTIFY_CLIENT_ID,
-   clientSECRET = process.env.SPOTIFY_CLIENT_SECRET,
-   state = 'mikeamysyedkenny';
-
-var spotifyApi = new SWA({
-   clientId: clientID,
-   clientSecret: clientSECRET,
-   redirectUri: 'http://localhost:1337/callback'
-});
-
-var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
-
-app.get('/spotify/login', (req, res) => {
-   console.log(authorizeURL);
-   res.cookie('auth_state', state);
-   res.redirect(authorizeURL);
-});
-
-app.get('/hey', (req, res) => {
-   console.log("here");
-   res.send('hi');
-});
-
-
-app.get('/callback', (req, res) => {
-   spotifyApi.authorizationCodeGrant(req.query.code).then(
-      (data) => {
-         let a_token = data.body['access_token'];
-         let r_token = data.body['refresh_token'];
-
-         console.log('The token expires in ' + data.body['expires_in']);
-         console.log('The access token is ' + a_token);
-         console.log('The refresh_token is ' + data.body['refresh_token']);
-
-         spotifyApi.setAccessToken(a_token);
-         spotifyApi.setRefreshToken(r_token);
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
-
-app.use(express.static(path.join('..', 'frontend', 'build'))).use(cookieParser());;
-
-
-// Home URL redirects to spotify login page
-const loginURL = '/spotify/login';
-app.get('/', function (req, res) {
-   res.redirect(loginURL);
-});
-
-
-
+   .use(cookieParser());
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,22 +33,14 @@ var scopes = ['user-read-private', 'user-read-email'],
    clientSECRET = process.env.SPOTIFY_CLIENT_SECRET,
    state = 'mikeamysyedkenny';
 
+
 var spotifyApi = new SWA({
    clientId: clientID,
    clientSecret: clientSECRET,
    redirectUri: 'http://localhost:1337/callback'
 });
-                              /**********
-                              *   AUTH  *
-                              **********/
+
 var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
-
-
-// Home URL redirects to spotify login page
-const loginURL = '/spotify/login';
-app.get('/', function (req, res) {
-   res.redirect(loginURL);
-});
 
 
 // Redirects to authorize URL
@@ -118,13 +48,6 @@ app.get('/spotify/login', (_, res) => {
    console.log(authorizeURL);
    res.cookie('auth_state', state);
    res.redirect(authorizeURL);
-});
-
-
-// Can probably delete?
-app.get('/hey', (req, res) => {
-   console.log("here");
-   res.send('hi');
 });
 
 
@@ -152,17 +75,11 @@ app.get('/callback', (req, res) => {
       }
    );
 });
-                              /**************
-                              *   SHUFFLE   *
-                              **************/
 
 
-                              /*****************
-                              *   FRIENDSYNC   *
-                              *****************/
-
-
-
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Shuffle
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -197,6 +114,7 @@ app.post('/friendsync/queue/add/:groupid&:songid', function (req, res) {
     FriendSync.add_to_queue(req.params.groupid, req.params.songid)
 })
 
+
 /**
  * Removes user from group
  * 
@@ -208,11 +126,9 @@ app.get('/friendsync/leave/:groupid&:userid', function (req, res) {
 });
 
 
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // Listen
->>>>>>> friendsync
+//////////////////////////////////////////////////////////////////////////////////////////////////
 app.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
