6a50471c7f088ebd6f726eebd920778dd34d372b
Kenny <oseleonose@gmail.com>
2020-06-21 20:32:28 +0000

    display playlists from spotify api

--- frontend/src/components/Shuffle.js
+++ frontend/src/components/Shuffle.js
@@ -1,5 +1,52 @@
 import React from "react";
-
+/* Playlist json format
+ {
+         "collaborative":false,
+         "description":"",
+         "external_urls":{
+            "spotify":"https://open.spotify.com/playlist/7qyJCAZ3xrFydyvjXZYZ5j"
+         },
+         "href":"https://api.spotify.com/v1/playlists/7qyJCAZ3xrFydyvjXZYZ5j",
+         "id":"7qyJCAZ3xrFydyvjXZYZ5j",
+         "images":[
+            {
+               "height":640,
+               "url":"<url of above size>",
+               "width":640
+            },
+            {
+               "height":300,
+               "url":"<url of above size>",
+               "width":300
+            },
+            {
+               "height":60,
+               "url":"<url of above size>",
+               "width":60
+            }
+         ],
+         "name":"MORNING EPs",
+         "owner":{
+            "display_name":"Kenny",
+            "external_urls":{
+               "spotify":"https://open.spotify.com/user/nfscny9k0324ybonfy4u6xu4z"
+            },
+            "href":"https://api.spotify.com/v1/users/nfscny9k0324ybonfy4u6xu4z",
+            "id":"nfscny9k0324ybonfy4u6xu4z",
+            "type":"user",
+            "uri":"spotify:user:nfscny9k0324ybonfy4u6xu4z"
+         },
+         "primary_color":null,
+         "public":true,
+         "snapshot_id":"MTksYjMzOTAyZDA5ODBmNGQ5ZjRjYTQ0YmUzZWZjZTdhYzZkMTQzOWE3Zg==",
+         "tracks":{
+            "href":"https://api.spotify.com/v1/playlists/7qyJCAZ3xrFydyvjXZYZ5j/tracks",
+            "total":62
+         },
+         "type":"playlist",
+         "uri":"spotify:playlist:7qyJCAZ3xrFydyvjXZYZ5j"
+      },
+ */
 function Shuffle({ playlists }) {
          return (
       <div>
@@ -9,10 +56,13 @@ function Shuffle({ playlists }) {
           <option value="artist">Shuffle by Artist</option>
         </select>
 
-        <div>
+       <div>
             {playlists.map((track) =>{ 
                return (
-               <button type="button"> { track } </button>
+                  <div>
+                     <button type="button"> { track.name } </button>
+                  </div>
+
                );
             })}
          </div>
