3475be560d8b67328b02be02294881ad786a3e0a
Kenny <oseleonose@gmail.com>
2020-06-21 05:42:17 +0000

    Amy ui merge finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 import Shuffle from './components/Shuffle';
 import MusicPlayer from './components/MusicPlayer'
+import Tabs from './components/Tabs';
+import FriendSyncTab from './components/FriendSyncTab';
+import MultiPlaylistTab from './components/MultiPlaylistTab'
 
 function App() {
    const [playlists, setPlaylists] = useState([]);
+
   return (
-    <div className="App">
-     <Shuffle 
-     playlists = {["abc", "123", "abc", "abc", "abc"]}/>
-    <MusicPlayer />
+     <div>
+      <Tabs>
+        <div label="Shuffle">
+          <Shuffle
+           playlists = {["kenny", "syed", "amy", "mike"]}/>
+        </div>
+        <div label="MultiPlaylist">
+        <MultiPlaylistTab>
+          </MultiPlaylistTab>
+        </div>
+        <div label="Friendsync">
+        <FriendSyncTab>
+          </FriendSyncTab>
+        </div>
+        <div label="VanillaPlaylist">
+          leaving empty until the other three are done
+        </div>
+      </Tabs>
+        <MusicPlayer />
     </div>
   );
 }
--- frontend/src/components/Shuffle.js
+++ frontend/src/components/Shuffle.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 function Shuffle({ playlists }) {
-   return (
-      <div>
-      {playlists.map((track) =>{
          return (
-            <h1> { track }</h1>
-         );
-      })}
+      <div>
+        <select name="options" id="options">
+          <option value="mood">Shuffle by Mood</option>
+          <option value="genre">Shuffle by Genre</option>
+          <option value="artist">Shuffle by Artist</option>
+        </select>
+
+        <div>
+            {playlists.map((track) =>{ 
+               return (
+               <button type="button"> { track } </button>
+               );
+            })}
+         </div>
       </div>
-   );
+
+    );
+         
 }
 
 export default Shuffle;
