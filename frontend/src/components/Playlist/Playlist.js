38b40f90f3fe4dbd3172c1105efe8ac3ebe80b93
Kenny <oseleonose@gmail.com>
2020-07-13 11:05:54 +0000

    added dialog for refreshing token

--- frontend/src/components/Playlist/Playlist.js
+++ frontend/src/components/Playlist/Playlist.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from "react";
 import PlaylistInner from "./PlaylistInner";
 import PlaylistOuter from "./PlaylistOuter";
+import RefreshDialog from "../RefreshDialog";
 
 function Playlist({ 
    spotifyApi,
    user,
 }) {
    const [pane, setPane] = useState("outer"); 
+   const [refresh, setRefresh] = useState(false);
    const [selectedPlaylist, setSelectedPlaylist] = useState(null); 
    const [playlists, setPlaylists] = useState(null);
    let token = spotifyApi.getAccessToken();
@@ -17,12 +19,19 @@ function Playlist({
                setPlaylists(data.body.items);
             },
             (err) => {
+               if (err.statusCode == 401) {
+                  setRefresh(true);
+               }
                console.log('frontend::Playlist.js spotifyApi.getUserPlaylists() failed. Error: ', err);
             });
       }
    }, [spotifyApi, token, pane]);
    return (
       <div>
+      {refresh && 
+         <RefreshDialog 
+            spotifyApi={spotifyApi} />}
+
       {pane === "outer" && playlists && 
          <PlaylistOuter 
             playlists={playlists} 
